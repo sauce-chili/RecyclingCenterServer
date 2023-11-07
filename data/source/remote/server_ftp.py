6db339a725c7829107a6bcc12cdda550765fc65c
Dmitry Smirnov <71555556+sauce-chili@users.noreply.github.com>
2023-11-07 03:20:36 +0300

    New features of ServerFTP
    - added the ability to navigate to a directory on the server (there is no protection against cyclic paths)
    - added a utilitarian function for obtaining parameters from a Yaml file

--- data/source/remote/server_ftp.py
+++ data/source/remote/server_ftp.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 from ftplib import FTP
+from pathlib import Path
+
+from domain.utils import get_arguments_from_yaml
 
 
 @dataclass
@@ -10,22 +13,57 @@ class ParamFTP:
 
 
 class ServerFTP:
-    _instance = None
+    __instance = None
 
     def __new__(cls, ftp_param: ParamFTP):
-        if cls._instance is None:
-            cls._instance = super(ServerFTP, cls).__new__(cls)
-            cls._instance.ftp = FTP(ftp_param.host)
-            cls._instance.ftp.login(user=ftp_param.username, passwd=ftp_param.password)
-        return cls._instance
+        if cls.__instance is None:
+            cls.__instance = super(ServerFTP, cls).__new__(cls)
+            cls.__instance.__ftp = FTP(ftp_param.host)
+            cls.__instance.__ftp.login(user=ftp_param.username, passwd=ftp_param.password)
+            cls.__instance.__param_ftp = ftp_param
+        return cls.__instance
 
-    def download_file(self, path_ftp_file: str, path_local_file: str):
+    def download_file(self, path_ftp_file: Path, path_local_file: Path):
+
+        for folder in path_ftp_file.parents:
+            self.__ftp.cwd(folder.name)
+
         with open(path_local_file, mode='wb') as local_file:
-            self.ftp.retrbinary('RETR ' + path_ftp_file, local_file.write)
+            self.__ftp.retrbinary('RETR ' + str(path_ftp_file.name), local_file.write)
 
-    def upload_file(self, path_local_file: str, path_ftp_file: str):
+    def upload_file(self, path_local_file: Path, path_ftp_file: Path):
+
+        for folder in path_ftp_file.parents:
+            self.__ftp.cwd(folder.name)
+
         with open(path_local_file, mode='rb') as local_file:
-            self.ftp.storbinary('STOR ' + path_ftp_file, local_file)
+            self.__ftp.storbinary('STOR ' + str(path_ftp_file.name), local_file)
+
+    def get_url_to_file(self, ftp_file_path: str):
+        return f"ftp://{self.__param_ftp.username}@{self.__param_ftp.host}/{ftp_file_path}"
 
     def __del__(self):
-        self.ftp.quit()
+        self.__ftp.quit()
+
+
+def get_ftp_server_param(yaml_param_ftp_server: Path):
+    root_key = 'FTP_SERVER_PARAM'
+
+    cfg = get_arguments_from_yaml(path_to_yaml_file=yaml_param_ftp_server, key=root_key)
+
+    return ParamFTP(
+        host=cfg['host'],
+        username=cfg['username'],
+        password=cfg['password']
+    )
+
+# p = get_ftp_server_param(Path('../../../app/configurations.yaml'))
+#
+# server = ServerFTP(p)
+#
+# file = Path('D:\\programms\\Python\\RecyclingCenterServer\\db\\buffer_images\\test1.png')
+#
+# server.upload_file(
+#     path_ftp_file=Path(f"/images/{file.name}"),
+#     path_local_file=file
+# )
