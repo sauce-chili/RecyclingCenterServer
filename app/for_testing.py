a2f6b1520f0d736edda2f5c798018093280405ea
sauce-chili <saucechilidev@gmail.com>
2023-11-10 01:01:13 +0000

    Changes file for run system from terminal

--- app/for_testing.py
+++ app/for_testing.py
@@ -3,7 +3,8 @@ from pathlib import Path
 from domain.models import PreservationApplicationForm
 from domain.use_cases.factory_usecases.factory_save_application_use_case import FactorySaveApplicationUseCase
 
-cfg = Path("configurations.yaml")
+
+cfg = Path("/home/rospromeco_controller/RecyclingCenterServer/app/configurations.yaml")
 
 save_use_case = FactorySaveApplicationUseCase(
     yaml_cfg_file=cfg
@@ -15,7 +16,7 @@ test_preservation_application_from = PreservationApplicationForm(
     operation_type='inbound',
     equipment_type='notebooks',
     camera_type=None,
-    scales_type='scales 2',
+    scales_type='Scale 2',
     weight_extra=0.15,
     weight_container=2,
 )
