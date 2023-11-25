eeeddb9969d9b2f149c204a8cea4b2c41eeee4fb
sauce-chili <saucechilidev@gmail.com>
2023-11-25 06:41:34 +0300

    Refusal of last frame buffer. Now using reopen Cap

--- data/source/device/cameras/ipcamera.py
+++ data/source/device/cameras/ipcamera.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from video_streem_buffer import LastFrameBuffer
 import cv2
 
 
@@ -11,8 +10,14 @@ class IpCamera:
 
         self.__stor_dir = path_storage_dir
 
-        self.__last_frame_buffer = LastFrameBuffer(url=url_camera)
-        self.__last_frame_buffer.start()
+        self.__cap = cv2.VideoCapture(url_camera)
+
+        if not self.__cap.isOpened():
+            raise cv2.error(f'Unable to access the camera at the specified address {url_camera}')
+        
+        self.__cap.release()
+        self.__url = url_camera
+        
 
     def __check_path_to_storage_dir(self, path: Path) -> None:
         if not path.exists():
@@ -28,15 +33,16 @@ class IpCamera:
 
     def take_photo(self, output_photo_name: str) -> Path:
 
-        success, frame = self.__last_frame_buffer.read()
+        self.__cap =  cv2.VideoCapture(self.__url)
+
+        success, frame = self.__cap.read()
 
         if not success:
             raise cv2.error(f'Failed to read frame')
 
         full_path = self.__stor_dir / output_photo_name
         cv2.imwrite(str(full_path), frame)
+        
+        self.__cap.release()
 
         return full_path
-
-    def __del__(self):
-        self.__last_frame_buffer.release()
